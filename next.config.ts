fc458a3cc67d7daa3bbcdc5c985884f0d2726a6d
Vip <professional.inbox.simo@gmail.com>
2026-03-14 07:34:09 +0000

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Required for @opennextjs/cloudflare deployment
-  // See: https://opennext.js.org/cloudflare
-  output: "standalone",
-};
-
-export default nextConfig;
